9663ef4055556edc0ab9012475ef965d7604186a
FranciszekW <franciow10@gmail.com>
2024-10-01 10:43:23 +0200

    Added removing corrupted images from data.

--- src/scripts/combined.py
+++ src/scripts/combined.py
@@ -1,14 +1,16 @@
-from filter_images import main as filter_images_main
+from manually_filter_images import main as filter_images_main
 from move_images import main as move_images_main
 from reindex_data_images import main as reindex_main
 from remove_image_duplicates import remove_from_data as remove_data_duplicates
 from remove_image_duplicates import remove_from_raw as remove_raw_duplicates
+from remove_corrupted_images import main as remove_corrupted_data
 
 def main():
     remove_raw_duplicates()
     filter_images_main()
     move_images_main()
     reindex_main()
+    remove_corrupted_data()
     remove_data_duplicates()
 
 if __name__ == "__main__":
