15d65c8782d711bc95b0f0fd4a64936dc3edbf44
FranciszekW <franciow10@gmail.com>
2024-10-12 19:32:14 +0200

    Filtering is done manually, so commented that. Moved reindexing images to the end, after duplicate removal.

--- src/scripts/combined.py
+++ src/scripts/combined.py
@@ -1,6 +1,6 @@
 from manually_filter_images import main as filter_images_main
 from move_images import main as move_images_main
-from reindex_data_images import main as reindex_main
+from reindex_images import main as reindex_images
 from reindex_raw_images import  main as reindex_raw
 from remove_image_duplicates import remove_from_images as remove_images_duplicates
 from remove_image_duplicates import remove_from_raw as remove_raw_duplicates
@@ -10,12 +10,12 @@ from convert_to_jpg import main as convert_to_jpg
 def main():
     remove_raw_duplicates()
     reindex_raw()
-    filter_images_main()
+    # filter_images_main()
     move_images_main()
-    reindex_main()
     remove_corrupted_images()
     remove_images_duplicates()
     convert_to_jpg()
+    reindex_images()
 
 if __name__ == "__main__":
     main()
